4563b9667bf8f894d6b78ffef9ffeabd37091cc3
Anton Zhyrnyi <a.zhyrney@gmail.com>
2024-02-03 16:25:20 +0000

    tests: clean up tests a bit

--- apps/mapgenerator/tests/test_mapgen.py
+++ apps/mapgenerator/tests/test_mapgen.py
@@ -80,31 +80,25 @@ def data_for_mapgen_uw():
                 {
                     "dominions_id": "1786",
                     "name": "Fir Bolg",
-                    "id": "6a10c26a-96dc-49c4-9663-75151a3a609a",
                     "for_nation": "(EA) Tir na n'Og",
-                    "quantity": 1,
                     "magic": {"fire": "2", "blood": "2"},
                 },
                 {
                     "dominions_id": "7",
                     "name": "Emerald Guard",
-                    "id": "02d84d0c-1cbd-41ed-98a6-b5949010155d",
                     "for_nation": "(EA) T'ien Ch'i",
-                    "quantity": 1,
                 },
             ],
             "units": [
                 {
                     "dominions_id": "105",
                     "name": "Woodhenge Druid",
-                    "id": "dc5e61de-3747-46b0-bd33-b361cedc78d9",
                     "for_nation": "(EA) Tir na n'Og",
                     "quantity": "10",
                 },
                 {
                     "dominions_id": "408",
                     "name": "Water Elemental",
-                    "id": "23925abc-40c0-4feb-9dcd-4b13d4d336a5",
                     "for_nation": "(EA) T'ien Ch'i",
                     "quantity": "10",
                 },
