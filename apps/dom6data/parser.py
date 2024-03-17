aba965ca526bd8f348a068dc92650c14c516d2ec
Anton Zhyrnyi <a.zhyrney@gmail.com>
2024-03-17 08:25:45 +0000

    feat: add changes, mods and maps

---
+++ apps/dom6data/parser.py
@@ -0,0 +1,104 @@
+import csv
+import glob
+import os
+import re
+
+from core.redis import get_redis_client
+from dom6data.models import Dom6Nation, Dom6Unit, DEBUG
+
+
+def parse_dom6_units():
+    client = get_redis_client()
+    pipeline = client.pipeline()
+    unit_ids = Dom6Unit.all_pks()
+    [Dom6Unit.delete(unit_id, pipeline=pipeline) for unit_id in unit_ids]
+    pipeline.execute()
+    nation_ids = Dom6Nation.all_pks()
+    [Dom6Nation.delete(nation_id, pipeline=pipeline) for nation_id in nation_ids]
+    pipeline.execute()
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(current_dir, "csvs/BaseU.csv"), "r", newline="") as csv_file:
+        reader = csv.DictReader(csv_file, delimiter="\t")
+        for row in reader:
+            unit = Dom6Unit(name=row["name"], dominions_id=row["id"])
+            unit.save(pipeline=pipeline)
+    pipeline.execute()
+
+    with open(
+        os.path.join(current_dir, "csvs/nations.csv"), "r", newline=""
+    ) as csv_file:
+        reader = csv.DictReader(csv_file, delimiter="\t")
+        for row in reader:
+            nation = Dom6Nation(
+                name=row["name"], dominions_id=row["id"], era=row["era"]
+            )
+            nation.save(pipeline=pipeline)
+    pipeline.execute()
+    pipeline.reset()
+
+
+def parse_dom6_dm_files():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    files_to_parse = glob.glob(os.path.join(current_dir, "mods/*.dm"))
+    pipeline = get_redis_client().pipeline()
+    units = Dom6Unit.find(Dom6Unit.mod == DEBUG).all()
+    [Dom6Unit.delete(unit.pk, pipeline=pipeline) for unit in units]
+    pipeline.execute()
+    nations = Dom6Nation.find(Dom6Nation.mod == DEBUG)
+    [Dom6Nation.delete(nation.pk, pipeline=pipeline) for nation in nations]
+    pipeline.execute()
+    for dmfile in files_to_parse:
+        with open(dmfile, "r") as file_content:
+            new_nation, new_monster = False, False
+            monster_id, monster_name = "", ""
+            nation_id, nation_name, nation_era = "", "", ""
+            mod = DEBUG
+            for line in file_content.readlines():
+                if not line.startswith("--"):
+                    if "#newmonster" in line:
+                        new_nation, new_monster = False, True
+                        nation_id, nation_name, nation_era = "", "", ""
+                        monster_id = re.findall(r"\d+", line)[0]
+                    elif "#selectnation" in line:
+                        new_nation, new_monster = True, False
+                        monster_id, monster_name = "", ""
+                        nation_id = re.findall(r"\d+", line)[0]
+                    elif "#end" in line:
+                        if new_monster and monster_name:
+                            unit = Dom6Unit(
+                                dominions_id=monster_id,
+                                name=monster_name,
+                                mod=mod,
+                            )
+                            unit.save(pipeline=pipeline)
+                        elif new_nation and nation_name:
+                            nation = Dom6Nation(
+                                dominions_id=nation_id,
+                                name=nation_name,
+                                era=nation_era,
+                                mod=mod,
+                            )
+                            nation.save(pipeline=pipeline)
+                        new_nation, new_monster = False, False
+                        monster_id, monster_name = "", ""
+                        nation_id, nation_name, nation_era = "", "", ""
+                    if new_monster or new_nation:
+                        if "#name" in line and "nametype" not in line:
+                            name = (
+                                " ".join(line.split(" ")[1:])
+                                .replace('"', "")
+                                .strip()
+                                .replace("\n", "")
+                            )
+                            if new_monster:
+                                monster_name = name
+                            elif new_nation:
+                                nation_name = name
+                        elif new_nation and "#era" in line:
+                            nation_era = (
+                                " ".join(line.split(" ")[1:])
+                                .replace('"', "")
+                                .replace("\n", "")
+                            )
+    pipeline.execute()
+    pipeline.reset()
--- parse_data.py
+++ parse_data.py
@@ -1,6 +1,7 @@
 import sys
 
 from apps.dom6data.loader import load_required_data
+from apps.dom6data.parser import parse_dom6_units, parse_dom6_dm_files
 from apps.domdata.parser import parse_dm_files, parse_units
 
 if __name__ == "__main__":
@@ -8,4 +9,6 @@ if __name__ == "__main__":
     parse_units()
     parse_dm_files()
     load_required_data()
+    parse_dom6_units()
+    parse_dom6_dm_files()
     sys.stdout.write("Parsing finished \n")
