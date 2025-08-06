40d1a66fdb399078419127045340214a35fd7aab
Anton Zhyrnyi <a.zhyrney@gmail.com>
2025-08-06 16:47:00 +0100

    feat: small tweaks and analytics

--- apps/mapgenerator/app.py
+++ apps/mapgenerator/app.py
@@ -42,7 +42,7 @@ async def init_sentry(_):
             enable_tracing=True,
             # Set traces_sample_rate to 1.0 to capture 100%
             # of transactions for performance monitoring.
-            traces_sample_rate=1.0,
+            traces_sample_rate=0.2,
         )
 
 
